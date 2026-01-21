8cdf6f0140a30ae2b0d5ca7030fad0ff5abb0b4e
edstevo <edward.stephenson@me.com>
2026-01-21 17:13:34 +0000

    - ShopifyGraphRequest base class

---
+++ src/ShopifyGraphRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EdStevo\LaravelShopifyGraph;
+
+use EdStevo\LaravelShopifyGraph\Facades\LaravelShopifyGraph;
+use Illuminate\Support\Facades\Log;
+
+abstract class ShopifyGraphRequest
+{
+    abstract public function query(): string;
+
+    abstract public function variables(): array;
+
+    abstract public function transformResponse(array $data): mixed;
+
+    public function post(string $shop, string $accessToken): mixed
+    {
+        try {
+            $res = LaravelShopifyGraph::post($shop, $accessToken, $this->query(), $this->variables());
+            return $this->transformResponse($res['data']);
+        } catch (\Exception $e) {
+            Log::debug("Shopify Graph Error", [
+                'error' => $e->getMessage()
+            ]);
+
+            throw $e;
+        }
+    }
+}
