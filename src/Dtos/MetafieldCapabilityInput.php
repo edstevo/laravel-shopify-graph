cce488f0b5ecfb161ee0e76ff0230c5ce6d58a82
edstevo <edward.stephenson@me.com>
2026-02-02 11:51:54 +0000

    - Update MetafieldCapabilityCreateInput.php

--- src/Dtos/MetafieldCapabilityInput.php
+++ src/Dtos/MetafieldCapabilityInput.php
@@ -9,15 +9,15 @@ class MetafieldCapabilityInput extends Data
     /**
      * The input for updating the admin filterable capability.
      */
-    public MetafieldCapabilityAdminFilterableInput $adminFilterable;
+    public ?MetafieldCapabilityAdminFilterableInput $adminFilterable = null;
 
     /**
-     * The input for updating the admin filterable capability.
+     * The input for updating the smart collection condition capability.
      */
-    public MetafieldCapabilitySmartCollectionConditionInput $smartCollectionCondition;
+    public ?MetafieldCapabilitySmartCollectionConditionInput $smartCollectionCondition = null;
 
     /**
-     * The input for updating the admin filterable capability.
+     * The input for updating the unique values capability.
      */
-    public MetafieldCapabilityUniqueValuesInput $uniqueValues;
+    public ?MetafieldCapabilityUniqueValuesInput $uniqueValues = null;
 }
