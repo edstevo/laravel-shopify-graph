b0c8127213be6fe2b3d92a762fdbe2583ad53e70
edstevo <edward.stephenson@me.com>
2026-02-04 21:26:12 +0000

    - fix ProductSetMutation

--- src/MutationInputs/ProductSetMutation.php
+++ src/MutationInputs/ProductSetMutation.php
@@ -12,10 +12,20 @@ class ProductSetMutation extends Data
     /**
      * Specifies the identifier that will be used to lookup the resource.
      */
-    public Optional|ProductSetIdentifiers $identifier;
+    public Optional|null|ProductSetIdentifiers $identifier;
 
     /**
      * The properties of the newly created or updated product.
      */
     public ProductSetInput $input;
+
+    /**
+     * Whether the mutation should be run synchronously or asynchronously.
+     * If true, the mutation will return the updated product.
+     * If false, the mutation will return a productSetOperation.
+     * Defaults to true.
+     * Setting synchronous: false may be desirable depending on the input complexity/size, and should be used if you are experiencing timeouts.
+     * Note: When run in the context of a bulk operation, the mutation will always run synchronously and this argument will be ignored.
+     */
+    public bool $synchronous;
 }
